34072a6c68baf8834b39250463490de0f6aa106f
Nilyesh <yamuna@yamunaindia.com>
2025-12-07 10:13:37 +0530

    Modify ALLOWED_HOSTS for HTTPS support
    Updated ALLOWED_HOSTS to include HTTPS and removed HTTP entries.

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,14 +28,10 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "yamuna-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
-]
+ALLOWED_HOSTS = ['https://localhost','https://yamuna-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://yamuna-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    'https://yamuna-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
