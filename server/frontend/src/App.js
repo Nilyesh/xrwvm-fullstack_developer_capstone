bbb07c4d5d15b39083d84e5a709053f74f155a42
Nilyesh <yamuna@yamunaindia.com>
2025-12-07 12:49:00 +0530

    Add routing for Home, Register, Dealers, and DealerDetails

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,10 +1,18 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Dealers from "./components/Dealers";
+import DealerDetails from "./components/DealerDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealer/:id" element={<DealerDetails />} />
     </Routes>
   );
 }
