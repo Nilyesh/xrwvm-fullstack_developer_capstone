0c0d873b00923ce5cba852c2983577706e79c71e
Nilyesh <yamuna@yamunaindia.com>
2025-12-07 13:18:28 +0530

    Add CarMake and CarModel models with admin registration

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -37,19 +37,30 @@ class CarModel(models.Model):
     
     # Other fields as needed
 
-    # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = mdoels.CharField(max_length=100)
+    description = model.TextField()
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    def __str__ (self):
+        return self.name   # method to print a car make object
 
+class CarModel(models.Model):
+car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) # - Many-To-One relationship 
+name = mdoels.CharField(max_length=100)
+CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon', 'TATA'),        
+type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+year = models.IntegerField(default=2023,
+        validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+def __str__ (self):
+        return self.name   # method to print a car make object
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ("name", "type", "year", "dealer_id", "car_make")
+
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
