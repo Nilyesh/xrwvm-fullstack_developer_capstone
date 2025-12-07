51f284d4aaa5eb671f1d199679ab9e487784921e
Nilyesh <yamuna@yamunaindia.com>
2025-12-07 14:14:02 +0530

    Update Dealers import and route to Dealer component

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -2,7 +2,7 @@ import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Register from "./components/Register";
-import Dealers from "./components/Dealers";
+import Dealers from "./components/Dealers/Dealer";
 import DealerDetails from "./components/DealerDetails";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/dealers" element={<Dealer/>} />
       <Route path="/dealer/:id" element={<DealerDetails />} />
     </Routes>
   );
