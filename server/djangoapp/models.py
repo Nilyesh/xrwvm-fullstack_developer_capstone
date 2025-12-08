33499afce5453946047229a70f91a4ed73b62c22
Nilyesh <yamuna@yamunaindia.com>
2025-12-08 15:52:07 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -46,11 +46,11 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
 car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) # - Many-To-One relationship 
-name = mdoels.CharField(max_length=100)
+name = models.CharField(max_length=100)
 CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
-        ('WAGON', 'Wagon', 'TATA'),        
+        ('WAGON', 'Wagon'),        
 type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
 year = models.IntegerField(default=2023,
         validators=[MinValueValidator(2015), MaxValueValidator(2023)])
