0813e03588d6728e645e774971b5010fa7f7735c
Nilyesh <yamuna@yamunaindia.com>
2025-12-09 21:59:33 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('dealers/', TemplateView.as_view(template_name="index.html"), name='dealers'),
     path('dealer/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='dealer_page'),
     path('postreview/<int:dealer_id>/', TemplateView.as_view(template_name="index.html"), name='post_review'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ]
 
 # Serve static and media files in development
