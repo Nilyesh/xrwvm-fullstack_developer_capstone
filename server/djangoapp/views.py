f09bfa3ef89fd7c706d21482b7f6594a84949924
https:/github.com/Nilyesh <https:/github.com/yamuna@yamunaindia.com>
2025-12-15 11:38:38 -0500

    changes made from theia lab environment

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
+from .populate import initiate
 import json
 import logging
 
